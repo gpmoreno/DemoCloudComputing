dc6c583385f59c75b3b4757c46cfb8bc19e3159b
Francisco Rubio <francisrubio06@hotmail.com>
2020-01-18 10:15:18 -0500

    database connection

--- database/connection.php
+++ database/connection.php
@@ -1,13 +1,8 @@
 <?php
     $servername = "localhost";
-<<<<<<< HEAD
     $username = "springstudent";
     $password = "springstudent";
     $dataBase = "cp_escolastico";
-=======
-    $username = "root";
-    $password = "";
->>>>>>> bd1313cf6c197aa03e69b0d27117eac99ebdb544
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dataBase);
