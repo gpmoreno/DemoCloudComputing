bf46dd6219ad6d1bfc26a55f5108edd91063f27b
Francisco Rubio <francisrubio06@hotmail.com>
2020-01-18 11:23:55 -0500

    agregar alumno parte1

--- views/nuevo.php
+++ views/nuevo.php
@@ -1,26 +1,36 @@
-<html>
-<link
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
       crossorigin="anonymous"
     />
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-<form class="form-horizontal">
 
-<fieldset>
-
-<!-- Form Name -->
-<legend>Nuevo Alumno</legend>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Agregar estudiante</title>
+</head>
+<body>
+<div class="container">
+  <h2>Agregar Alumnos</h2>
+  <hr>
+  <form>
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput">Cédula</label>  
   <div class="col-md-5">
   <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
-    
   </div>
 </div>
 
@@ -29,7 +39,6 @@
   <label class="col-md-4 control-label" for="textinput">Edad</label>  
   <div class="col-md-4">
   <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
-    
   </div>
 </div>
 
@@ -38,7 +47,6 @@
   <label class="col-md-4 control-label" for="textinput">Teléfono</label>  
   <div class="col-md-5">
   <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
-    
   </div>
 </div>
 
@@ -47,7 +55,6 @@
   <label class="col-md-4 control-label" for="textinput">Nombre</label>  
   <div class="col-md-5">
   <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
-    
   </div>
 </div>
 
@@ -58,13 +65,12 @@
     <button id="singlebutton" name="singlebutton" class="btn btn-primary">Guardar</button>
   </div>
 </div>
+  </form>
+</div>
 
-</fieldset>
-</form>
-</div>
-</div>
-</div>
-<script
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
       src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
       integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
       crossorigin="anonymous"
@@ -79,4 +85,5 @@
       integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
       crossorigin="anonymous"
     ></script>
-    </html>
+</body>
+</html>
