b46ed9b4693ea6ea7b9638f76bf100e74c03778a
gpmoreno <gp.moreno1992@gmail.com>
2020-01-18 10:40:39 -0600

    query

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "springstudent";
-    $password = "springstudent";
+    $username = "root";
+    $password = "";
     $dataBase = "cp_escolastico";
 
 // Create connection
@@ -11,5 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dataBase);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?> 
